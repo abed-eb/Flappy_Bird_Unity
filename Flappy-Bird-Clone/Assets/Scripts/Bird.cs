3a7c5c3c6af065f0d41e9f1c8e5c64bc6deb0048
abed ebadi <abdi.eb79@gmail.com>
2021-11-21 17:51:19 +0330

    Worked on bird and background

---
+++ Flappy-Bird-Clone/Assets/Scripts/Bird.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    [SerializeField]
+    private float _jumoAmount = 100f;
+
+    [SerializeField] private Rigidbody2D _birdRigidBody2D;
+    
+    [SerializeField]
+    private bool _startGame = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void Awake()
+    {
+        _birdRigidBody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+            _startGame = true;
+        }
+
+        if (_startGame == true)
+        {
+            // _birdRigidBody2D.velocity = Vector2.right * Time.deltaTime;
+        }
+        
+        
+    }
+
+    private void Jump()
+    {
+        _birdRigidBody2D.velocity = Vector2.up * _jumoAmount;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        
+    }
+}
---
+++ Flappy-Bird-Clone/Assets/Scripts/Level.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    public GameObject pipe_up;
+    public GameObject pipe_down;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void CreatePipe()
+    {
+       // Instantiate(pipe_up, new Vector3()) 
+    }
+}
